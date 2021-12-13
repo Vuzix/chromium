01c74305840095220f7005e458a131426d810cab
ansoncheung-vuzix <anson_cheung@vuzix.com>
2021-12-13 10:13:16 -0500

    Fix webview return wrong camera resolution problem.
    The problem is caused by the switch between portrait and landscape mode.
    
    No need to consider to switch between portrait and landscape mode. Smart glasses only support landscape, so, no need to swith w and h.
    
    VUZIX M400-2483

--- media/capture/video/android/java/src/org/chromium/media/VideoCaptureCamera2.java
+++ media/capture/video/android/java/src/org/chromium/media/VideoCaptureCamera2.java
@@ -1777,6 +1777,9 @@ public class VideoCaptureCamera2 extends VideoCapture {
         // orientation.
         int capture_width = width;
         int capture_height = height;
+        // VUZIX M400-2483: No need to consider to switch between portrait and landscape mode.
+        // Smart glasses only support landscape, so, no need to swith w and h.
+        /*
         if (mCameraNativeOrientation == 0 || mCameraNativeOrientation == 180) {
             Log.d(
                     TAG,
@@ -1784,6 +1787,8 @@ public class VideoCaptureCamera2 extends VideoCapture {
             capture_width = height;
             capture_height = width;
         }
+        */
+        // End VUZIX change
 
         // Find closest supported size.
         final Size[] supportedSizes = streamMap.getOutputSizes(ImageFormat.YUV_420_888);
